7e484707e94c65ce7d3669b6b4ff652820409d31
Abdelmounim Sedoun <asedoun@e3r1p1.1337.ma>
2025-09-20 04:35:22 +0100

    starting the games selection page

--- front-end/src/app/Home/Games/page.tsx
+++ front-end/src/app/Home/Games/page.tsx
@@ -1,12 +1,88 @@
+import "../../globals.css";
 export default function Home() {
   return (
-    <div className="h-[calc(100%-232px)] flex flex-col items-center justify-center">
-      <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
-          Coming Soon
-      </h1>
-      <p className="text-white text-lg mb-8">
-          We're working hard to bring you something amazing. Stay tuned!
-      </p>
+    <>
+     <div
+   className="
+   absolute
+   w-[20vw] h-[60vh]  /* increased from 50vh */
+   top-[20vh] left-[10vw]
+   rounded-[2vw]
+   rotate-0
+   opacity-[0.8]
+   bg-cover bg-center
+   shadow-[0_0_2vw_rgba(0,0,0,0.5)]
+ "
+      style={{ backgroundImage: "url('/images/localPong.png')" }}
+    >  
+    <span
+    className="
+    absolute
+    left-1/2 bottom-0
+    -translate-x-1/2 translate-y-1/3
+    font-poppins font-semibold
+    text-[32px] leading-[100%] tracking-[0%]
+    text-white
+  "
+  >
+    Masters Arena
+  </span>
     </div>
+   <div
+  id="OnlineTourPong"
+  className="
+    absolute
+    w-[22vw] h-[65vh]  /* increased from 55vh */
+    top-[18vh] left-[40vw]
+    rounded-[2vw]
+    rotate-0
+    opacity-100
+    bg-cover bg-center
+    shadow-[0_0_2vw_rgba(0,0,0,0.5)]
+  "
+  style={{ backgroundImage: "url('/images/onlineAndTour.png')" }}
+  
+>
+<span
+    className="
+      absolute
+      left-1/2 bottom-0
+      -translate-x-1/2 translate-y-1/3
+      font-poppins font-semibold
+      text-[36px] leading-[100%] tracking-[0%]
+      text-white
+    "
+  >
+    Head To Head
+  </span>
+</div >
+    <div    className="
+    absolute
+    w-[20vw] h-[60vh]  /* increased from 50vh */
+    top-[20vh] left-[70vw]
+    rounded-[2vw]
+    rotate-0
+    opacity-[0.65]
+    bg-cover bg-center
+    shadow-[0_0_2vw_rgba(0,0,0,0.5)]
+  " id="HandClash"
+  style={{ backgroundImage: "url('/images/rps.png')" }}
+  >
+    <span
+    className="
+      absolute
+      left-1/2 bottom-0
+      -translate-x-1/2 translate-y-1/3
+      font-poppins font-semibold
+      text-[32px] leading-[100%] tracking-[0%]
+      text-white
+    "
+
+  >
+    Hand Clash
+  </span>
+  </div>
+  
+    </>
   );
 }
