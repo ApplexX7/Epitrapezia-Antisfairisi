46c246626f5b91b1363f7bdb0a0fa7f6f9f20342
Abdelmounim Sedoun <asedoun@e3r1p17.1337.ma>
2025-09-24 10:08:39 +0100

    conditional layout for Local and Online pong routes

--- front-end/src/app/Home/layout.tsx
+++ front-end/src/app/Home/layout.tsx
@@ -1,19 +1,23 @@
+"use client";
+
 import "../globals.css";
-import Link from "next/link";
-import Image from "next/image";
-import { NavBar } from '@/components/Navbar'
+import { usePathname } from "next/navigation";
 import HomeNavBar from "@/components/HomeNavBar";
 
-
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const noLayoutRoutes = ["/Home/Games/LocalPong", "/Home/Games/OnlinePong"];
+
+  if (noLayoutRoutes.includes(pathname)) {
+    return <>{children}</>;
+  }
+
   return (
-      <div className="custom-gradient h-screen w-full 
-      flex flex-col items-center">
-        <div className="flex flex-col  max-w-480 size-full">
-          <HomeNavBar />
-          {children}
-        </div>
+    <div className="custom-gradient h-screen w-full flex flex-col items-center">
+      <div className="flex flex-col max-w-480 size-full">
+        <HomeNavBar />
+        {children}
       </div>
+    </div>
   );
 }
