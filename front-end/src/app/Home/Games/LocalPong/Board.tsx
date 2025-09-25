4c6487e09bb87bc5ad58acdfdec3c3e0de92d654
Abdelmounim Sedoun <asedoun@e3r1p17.1337.ma>
2025-09-25 16:05:13 +0100

    adding game counter

--- front-end/src/app/Home/Games/LocalPong/Board.tsx
+++ front-end/src/app/Home/Games/LocalPong/Board.tsx
@@ -1,4 +1,20 @@
+'use client'
+import { useState , useEffect} from "react";
+
+
 export default function Board() {
+  const [startGameCounter, setStartGameCounter] = useState(5);
+
+useEffect(() => {
+  if (startGameCounter <= 0) return;
+
+  const intervalId = setInterval(() => {
+    setStartGameCounter(prev => prev - 1);
+  }, 1000);
+
+  return () => clearInterval(intervalId);
+}, [startGameCounter]);
+
   return (
     <div
       className="relative m-auto w-[80vw] h-[75vh]"
@@ -8,15 +24,26 @@ export default function Board() {
         className="absolute left-0 top-1/2 w-[19px] h-[20%] rounded-sm -translate-y-1/2"
         style={{ backgroundColor: "#FF007F" }}
       ></div>
-      <div className="absolute left-1/2 top-0 h-full w-[1%] -translate-x-1/2 bg-white-smoke" >
-      </div>
-      <div className="absolute left-1/2 top-1/2 w-[24px] h-[24px] -translate-x-1/2 -translate-y-1/2 rounded-full" style={{backgroundColor : "#FF007F"}}>
-
-      </div>
+      <div className="absolute left-1/2 top-0 h-full w-[1%] -translate-x-1/2 bg-white-smoke"></div>
+      <div
+        className="absolute left-1/2 top-1/2 w-[24px] h-[24px] -translate-x-1/2 -translate-y-1/2 rounded-full"
+        style={{ backgroundColor: "#FF007F" }}
+      ></div>
       <div
         className="absolute right-0 top-1/2 w-[19px] h-[20%] rounded-sm -translate-y-1/2"
         style={{ backgroundColor: "#FF007F" }}
       ></div>
+      {startGameCounter > 0 ?
+      <p
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   font-extrabold text-white opacity-80  transition-all duration-200"
+        style={{
+          fontSize: "15vh",
+          lineHeight: "1",
+        }}
+      >
+        {startGameCounter}
+      </p> : null}
     </div>
   );
 }
