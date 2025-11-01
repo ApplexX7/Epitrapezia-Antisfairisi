e0af02279f17fd0f15bae9b37e7452da55af4dfb
Abdelmounim Sedoun <asedoun@e3r11p8.1337.ma>
2025-11-01 13:28:37 +0100

    cleanup

--- Backend/src/routers/player.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from "../server";
-import { playerAvatare } from "../controllers/playerAvatare";
-import { UserSearch } from "../controllers/UserSearch";
-
-
-export function playerSettings(){
-    Server.instance().post("/settings/avatar", playerAvatare());
-    Server.instance().get("/search", UserSearch());
-}
--- front-end/src/components/loadingComp.tsx
+++
@@ -1,9 +0,0 @@
-import { Loading } from "@/components/ui/progress"
-
-export default function LoadingComp(){
-    return (
-        <div className="flex custom-gradient justify-center items-center h-screen">
-        <Loading /> 
-      </div>
-    )
-}
