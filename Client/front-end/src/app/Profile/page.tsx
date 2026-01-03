95d923e68754f58f5ad8f7220ebbadde4026e47f
Applex <154215979+ApplexX7@users.noreply.github.com>
2026-01-03 20:29:10 +0000

    Update ui for profile

--- Client/front-end/src/app/Profile/page.tsx
+++ Client/front-end/src/app/Profile/page.tsx
@@ -80,7 +80,7 @@ export default function Profile() {
         bg-gradient-to-r from-white-smoke to-white-smoke/60
         text-transparent bg-clip-text z-10" >{isOwnProfile ? 'My Account' : `${profileUser?.username}'s Profile`}</h1>
       <div className="-mt-4 grid grid-cols-4 
-        gap-5 w-full h-[calc(100%-232px)] p-5  auto-rows-min">
+        gap-5 w-full h-[calc(100vh-10rem)] p-5  auto-rows-min overflow-y-auto">
           <BoxLayout className="w-full h-ful   card col-span-3 row-span-2">
             <Playerinfo user={profileUser}/>
           </BoxLayout>
