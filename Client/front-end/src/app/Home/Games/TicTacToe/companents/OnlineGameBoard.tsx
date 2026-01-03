7764d0ba6c5bbc91f3e24266e81fe87a03b71704
Abdelmounim Sedoun <149968190+0xCheckMate@users.noreply.github.com>
2026-01-03 15:59:42 +0000

    ..

---
+++ Client/front-end/src/app/Home/Games/TicTacToe/companents/OnlineGameBoard.tsx
@@ -0,0 +1,29 @@
+import OnlineSquare from './OnlineSquare';
+
+interface OnlineGameBoardProps {
+  board: (string | null)[];
+  winner: { player: string; line: number[] } | null;
+  onCellClick: (index: number) => void;
+  disabled?: boolean;
+}
+
+export default function OnlineGameBoard({ board, winner, onCellClick, disabled }: OnlineGameBoardProps) {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {board.map((value, index) => {
+        const isWinningCell = winner?.line?.includes(index);
+        return (
+          <OnlineSquare
+            key={index}
+            value={value}
+            index={index}
+            isWinningCell={!!isWinningCell}
+            hasWinner={!!winner}
+            onClick={onCellClick}
+            disabled={disabled}
+          />
+        );
+      })}
+    </div>
+  );
+}
