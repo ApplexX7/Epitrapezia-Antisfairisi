b85bb2c16b56175d349d322cac78ad181a6f51f4
Applex <154215979+ApplexX7@users.noreply.github.com>
2026-01-06 23:42:18 +0000

    Update the devops part

--- Server/Backend/src/index.ts
+++ Server/Backend/src/index.ts
@@ -40,7 +40,7 @@ const httpRequetsTotal = new client.Counter({
 const httpsRequestsDuration = new client.Histogram({
   name: "http_request_duration_seconds",
   help: "Duration of HTTP requests in seconds",
-  labelNames: ["method", "route", "status"],
+  labelNames: ["method", "route", "status_code"],
   buckets: [0.1, 0.5, 1, 1.5, 2, 5],
 });
 
